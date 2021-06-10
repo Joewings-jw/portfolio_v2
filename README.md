6021ee9fe6a49295c7d358b93ecb91ab3c1cdf70
Joewings-jw <ngugijoseph830@gmail.com>
2021-06-10 18:42:35 +0300

    parcel.js compiler

---
+++ README.md
@@ -0,0 +1,31 @@
+### Responsive portfolio website
+
+### Tools used
+#### parcel
+
+Parcel supports many different languages and file types out of the box, from web technologies like HTML, CSS, and JavaScript, to lower level languages like Rust, and anything that compiles to WebAssembly (WASM), to assets like images, fonts, videos, and more.
+
+Parcel makes your code portable. You can build your code for different environments, for the web for your server, or for an app. You can even build multiple targets at once and have them live update as you make changes.
+
+### Package manager used
+### yarn
+
+### To run this project run;
+
+#### git clone the repo
+
+#### run yarn install
+
+#### yarn start to serve on localhost:1234/ on dev mode
+
+#### yarn build to Build code once, in production mode.
+
+##### previews
+
+![Alt text](assets/img/previews/progress1.png?raw=true)
+
+
+![Alt text](assets/img/previews/progress2.png?raw=true)
+
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,176 @@
+
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
+//MENU SHOW 
+
+if(navToggle){
+navToggle.addEventListener('click', ()=>{
+    navMenu.classList.add('show-menu')
+});
+}
+//MENU HIDDEN
+if(navClose){
+navClose.addEventListener('click', ()=>{
+    navMenu.classList.remove('show-menu')
+});
+}
+
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+const navMenu = document.getElementById('nav-menu')
+// When we click on each nav_link, we remove the show-menu class
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+//SERVICES MODAL
+const modalViews = document.querySelectorAll('.services_modal'),
+modalBtns = document.querySelectorAll('.services_button'),
+modalCloses = document.querySelectorAll('.services_modal-close')
+
+
+let modal = function(modalClick){
+modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i)=>{
+modalBtn.addEventListener('click', ()=>{
+    modal(i)
+})
+})
+
+modalCloses.forEach((modalClose)=>{
+modalClose.addEventListener('click', ()=>{
+    modalViews.forEach((modalView)=>{
+        modalView.classList.remove('active-modal');
+    })
+})
+})
+
+// PORTFOLIO SWIPER
+
+let swiperPortfolio = new Swiper(".portfolio_container", {
+// cssMode: true,
+// loop: true,
+effect:"cube",
+grabCursor: true,
+cubeEffect: {
+  shadow: true,
+  slideShadows: true,
+  shadowOffset: 20,
+  shadowScale: 0.64,
+},
+navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+},
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+},
+
+});
+
+
+let swiperTestimonial = new Swiper(".testimonial_container", {
+loop: true,
+grabCursor: true,
+spaceBetween: 48,
+
+
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  dynamicBullets: true,
+
+},
+breakPoints: {
+    568:{
+        width: 568,
+        slidesPerView: 1,
+    }, 
+    768:{
+        width: 768,
+        slidesPerView: 2,
+    },
+    1024:{
+        width: 1024,
+        slidesPerView: 2,
+    }
+    
+}
+
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+const scrollY = window.pageYOffset
+
+sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+        document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+const nav = document.getElementById('header')
+// When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+//show scroll
+function scrollUp(){
+const scrollup = document.getElementById('scroll-up');
+// When the scroll is higher than 80 viewport height, add the show-scroll class to the a tag with the scroll-top class
+if(this.scrollY >= 80) scrollup.classList.add('show-scroll'); else scrollup.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+//Dark theme
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+// Add or remove the dark / icon theme
+document.body.classList.toggle(darkTheme)
+themeButton.classList.toggle(iconTheme)
+// We save the theme and the current icon that the user chose
+localStorage.setItem('selected-theme', getCurrentTheme())
+localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
